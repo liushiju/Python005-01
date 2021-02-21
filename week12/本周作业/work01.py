dfed76bfd59cf8912c16fead0265f490b2f609f4
liushiju <lsj_neusoft@163.com>
2021-02-22 03:05:59 +0800

    本周作业

--- week12/本周作业/work01.py
+++ week12/本周作业/work01.py
@@ -3,11 +3,12 @@ import csv
 import requests
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
+from collections import Counter
 
 '''
 安装并使用 requests、bs4 库，爬取猫眼电影的前 10 个电影名称、电影类型和上映时间，并以 UTF-8 字符集保存到 csv 格式的文件中。
 '''
-proxies = {"http": "58.220.95.86:9401"}
+
 ua = UserAgent()
 
 
@@ -19,9 +20,12 @@ def crawl_maoyan(url):
         "User-Agent": ua.random,
         "Cache - Control": "no - cache",
         "Connection": "keep - alive",
+        "Host": "maoyan.com",
     }
-    response = requests.get(url, headers=headers, proxies=proxies)
+    response = requests.get(url, headers=headers)
+    # print(response.status_code)
     bs_info = bs(response.text, 'html.parser')
+    # print(bs_info)
     return bs_info
 
 
@@ -29,37 +33,39 @@ def get_htmls(url):
     bs_info = crawl_maoyan(url)
     baseurl = 'https://maoyan.com'
     urls = []
-    all_urls = set(urls)
     for tags in bs_info.find_all('div', attrs={'class': 'main'}):
         for atag in tags.find_all('a'):
             url = baseurl + atag.get('href')
             urls.append(url)
-    print(urls)
-    return all_urls
+    return urls
 
 
 def parse(url):
-    urls = get_htmls(url)
-    print(urls)
+    all_urls = get_htmls(url)
+    htmls = Counter(all_urls)
+    urls = sorted(htmls)
     result = []
     for url in urls:
         bs_info = crawl_maoyan(url)
-        for tags in bs_info.find_all('div', attrs={'class="movie-brief-container"'}):
+        for tags in bs_info.find_all('div', attrs={'class':'movie-brief-container'}):
             # 获取电影名称
             movie_name = tags.find('h1').text
             # 获取电影类型
-            movie_type = tags.find('a').text
-            # 上映时间
-            movie_time = tags.find('li[3]').text
+            movie_t = tags.find('li').text
+            movie_type = movie_t.replace('\n','、').strip('、')
+            # # 上映时间
+            movie_t = tags.find('ul').text
+            movie_time = movie_t.split()[0]
             res = [f"{movie_name}", f"{movie_type}", f"{movie_time}"]
             result.append(res)
+    print(result)
     return result
 
 
 def save_to_csv(url):
     result = parse(url)
     title = ['电影名称', '电影类型', '上映时间']
-    with open("猫眼TOP10.csv", 'w', newline='', encoding='gbk') as t:
+    with open("猫眼TOP10.csv", 'w', newline='', encoding='utf-8') as t:
         writer = csv.writer(t)
         writer.writerow(title)
         writer.writerows(result)
